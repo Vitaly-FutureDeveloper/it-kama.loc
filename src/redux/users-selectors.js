97cba3058157a1e04cf5856d25e845c23f6c5082
Vitali-FutureDeveloper <zarvoron@yandex.ru>
2022-03-15 01:12:35 +0500

    Поправили селекторы в users-selector

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -8,22 +8,37 @@ export const getUsers = createSelector(getUsersSelector,(users) => {
 	return users.filter(u => true);
 });
 
-export const getPageSize = (state) => {
+const getPageSizeSelector = (state) => {
 	return state.usersPage.pageSize;
 };
+export const getPageSize = createSelector(getPageSizeSelector, (pageSize) =>{
+	return pageSize;
+});
 
-export const getTotalUsersCount = (state) => {
+const getTotalUsersCountSelector = (state) => {
 	return state.usersPage.totalUsersCount;
 };
+export const getTotalUsersCount = createSelector(getTotalUsersCountSelector, (totalUsersCount) =>{
+	return totalUsersCount;
+});
 
-export const getCurrentPage = (state) => {
+const getCurrentPageSelector = (state) => {
 	return state.usersPage.currentPage;
 };
+export const getCurrentPage = createSelector(getCurrentPageSelector, (currentPage) =>{
+	return currentPage;
+});
 
-export const getIsFetching = (state) => {
+const getIsFetchingSelector = (state) => {
 	return state.usersPage.isFetching;
 };
+export const getIsFetching = createSelector(getIsFetchingSelector, (isFetching) =>{
+	return isFetching;
+});
 
-export const getFollowingInProgress = (state) => {
+const getFollowingInProgressSelector = (state) => {
 	return state.usersPage.followingInProgress;
 };
+export const getFollowingInProgress = createSelector(getFollowingInProgressSelector, (followingInProgress) =>{
+	return followingInProgress;
+});
